233cb769bbd77a63e6805e809709ae8458d8c6a9
vyazemskaya <vyazemskayanna@gmail.com>
2024-06-18 23:17:29 +0300

    team page

--- src/js/lib/swiper.js
+++ src/js/lib/swiper.js
@@ -1,7 +1,38 @@
 import Swiper from "swiper";
 import "swiper/css";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
-const initSliders = () => {};
+const setSlideContent = (slide) => {
+  document.querySelector(".team__name").innerHTML = slide.dataset.name;
+  document.querySelector(".team__text_team").innerHTML = slide.dataset.team;
+  document.querySelector(".team__text_date").innerHTML = slide.dataset.date;
+};
+
+const initSliders = () => {
+  if (document.querySelector(".team__swiper")) {
+    new Swiper(".team__swiper", {
+      modules: [Navigation, Pagination],
+      loop: true,
+      spaceBetween: 30,
+      navigation: {
+        prevEl: ".team__nav-btn_prev",
+        nextEl: ".team__nav-btn_next",
+      },
+      pagination: {
+        el: ".team__fraction",
+        type: "custom",
+        renderCustom: function (swiper, current, total) {
+          return current + "/" + total;
+        },
+      },
+      on: {
+        afterInit: (swiper) =>
+          setSlideContent(swiper.slides[swiper.activeIndex]),
+        slideChange: (swiper) =>
+          setSlideContent(swiper.slides[swiper.activeIndex]),
+      },
+    });
+  }
+};
 
 window.addEventListener("load", initSliders);
